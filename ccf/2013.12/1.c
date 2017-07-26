441542ea208d99f8c69d7d190a9b361a92027ed4
caoliqingstudio <qdcaoliqing@126.com>
2017-07-26 08:45:49 +0800

    整理

--- ccf/2013.12/1.c
+++ ccf/2013.12/1.c
@@ -1,57 +1,58 @@
 #include <stdio.h>
-int num=0;
-int move(int i,int **a,int temp){
-    int j=num;
-    for(;i<j;--j){
-        a[j][0]=a[j-1][0];
-        a[j][1]=a[j-1][1];
+int num = 1;
+int a[1001][2];
+int move(int i, int temp) {
+    int j = num;
+    for (; i<j; --j) {
+        a[j][0] = a[j - 1][0];
+        a[j][1] = a[j - 1][1];
     }
     num++;
-    a[i][0]=temp;
-    a[i][1]=1;
+    a[i][0] = temp;
+    a[i][1] = 1;
     return 0;
 }
-int main(){
-    int n,i,j,k,m,temp;
-    int a[1001][2]={0};
-    scanf("%d",&n);
-    scanf("%d",&a[0][0]);
-    a[0][0]=1;
-    for(i=1;i<n;++i){
-        scanf("%d",&temp);
-        if(temp<a[0][0]){
-            move(0,a,temp);
-        }else if(temp>a[num][0]){
-            a[num][0]=temp;
-            a[num][1]=1;
-        }else{
-            for(j=0,k==num;j<=k;){
-                m=(j+k)/2;
-                if(a[m][0]<temp){
-                    j=m+1;
-                }else if(a[m][0]==temp){
+int main() {
+    int n, i, j, k, m, temp;
+    scanf("%d", &n);
+    scanf("%d", &a[0][0]);
+    a[0][1] = 1;
+    for (i = 1; i<n; ++i) {
+        scanf("%d", &temp);
+        if (temp<a[0][0]) {
+            move(0, temp);
+        }
+        else if (temp>a[num-1][0]) {
+            a[num][0] = temp;
+            a[num][1] = 1;
+            ++num;
+        }
+        else {
+            for (j = 0, k = num-1; j <= k;) {
+                m = (j + k) / 2;
+                if (a[m][0]<temp) {
+                    j = m + 1;
+                }
+                else if (a[m][0] == temp) {
                     ++a[m][1];
                     break;
-                }else{
-                    k=m-1;
+                }
+                else {
+                    k = m - 1;
                 }
             }
-            if(a[m][0]!=temp){
-                move(j,a,temp);
+            if (a[m][0] != temp) {
+                move(j, temp);
             }
         }
     }
-    j=0;
-    for(i=0;i<=num;++i){
-        if(a[i][1]>j){
-            j=a[i][1];
-            k=a[i][0];
-        }else if(a[i][1]==j){
-            if(a[i][0]<k){
-                k=a[i][0];
-            }
+    j = 0;
+    for (i = 0; i < num; ++i) {
+        if (a[i][1]>j) {
+            j = a[i][1];
+            k = a[i][0];
         }
     }
-    printf("%d",k);
+    printf("%d", k);
     return 0;
 }
